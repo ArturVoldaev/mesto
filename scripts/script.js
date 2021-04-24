3de1de140a38b5eae476257747e0e62a2451d9ae
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-04-24 11:19:03 +0300

    Выполнить правки после первой итерации

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,34 @@
 let openButtonClick = document.querySelector(".user__edit");
 let PopUp = document.querySelector(".popup");
-
 let closeButtonClick = document.querySelector(".popup__close");
-
-function open () {
-  PopUp.classList.toggle("popup__non");
-}
-
-openButtonClick.addEventListener("click" , open);
-
-closeButtonClick.addEventListener("click" , open);
-
-let edit1 = document.querySelector(".user__name").innerHTML;
-let input1 = document.getElementById('name__form');
-input1.setAttribute('value', edit1);
-
-let edit2 = document.querySelector(".user__profession").innerHTML;
-let input2 = document.getElementById('profession__form');
-input2.setAttribute('value', edit2);
-
 let formElement = document.querySelector(".form");
 let nameInput = document.getElementById('name__form');
 let jobInput = document.getElementById('profession__form');
-
 let po = document.querySelector(".user__name");
 let op = document.querySelector(".user__profession");
+let submit = document.querySelector(".form");
 
-/*console.log(jobInput.value);*/
+function open () {
+  nameInput.value = po.textContent;
+  jobInput.value = op.textContent;
+  PopUp.classList.toggle("popup_non");
+}
+
+function close () {
+  PopUp.classList.remove("popup_non");
+}
+/*let edit1 = document.querySelector(".user__name");*/
+/*let input1 = document.getElementById('name__form');*/
+/*let edit2 = document.querySelector(".user__profession");*/
+/*let input2 = document.getElementById('profession__form');*/
 
 function formSubmitHandler (ev) {
   ev.preventDefault();
   po.textContent = nameInput.value;
   op.textContent = jobInput.value;
-  open ()
+  close ()
 }
 
-let submit = document.querySelector(".popup__window");
 submit.addEventListener('submit', formSubmitHandler);
+openButtonClick.addEventListener("click" , open);
+closeButtonClick.addEventListener("click" , close);
